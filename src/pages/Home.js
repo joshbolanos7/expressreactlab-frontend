d8b5cf6abce9b287be900fe92ec8ee3e3c6274e7
Joshua Bolanos <josh.bolanos7@gmail.com>
2021-10-18 23:07:26 -0400

    added some styling.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 
-function Home(props) {
+import background from '../images/eat-code-sleep-repeat.jpeg'
 
+console.log(background);
+function Home(props) {
     return (
         <div className="home">
             <h1>Hello and Welcome to my Portfolio! Click any of the links above to navigate yourself around the webpage. </h1>
-            
+            <img src={background} className='esrpic'/>
         </div>
     )
 }
